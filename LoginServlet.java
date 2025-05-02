38b8ea24e34e01bb792ee7af6ab4a06523086647
Jcsmst11 <jayani21k@gmail.com>
2025-05-02 16:17:12 +0530

    Update LoginServlet.java

--- LoginServlet.java
+++ LoginServlet.java
@@ -15,5 +15,40 @@ public class LoginServlet extends HttpServlet {
         //    - Store username in cookie
         //    - Redirect to DashboardServlet
         // 4. If invalid, redirect back to login.html
+
+        String username = request.getParameter("username");
+            String password = request.getParameter("password");
+        
+            if ("student1".equals(username) && "pass1".equals(password)) {
+              HttpSession session = request.getSession();
+              session.setAttribute("user", username);
+        
+              Cookie cookie = new Cookie("user", username);
+              response.addCookie(cookie);
+        
+              response.sendRedirect("DashboardServlet");
+            } else {
+              response.getWriter().println("Invalid ");
+            }
+
+            HttpSession session = request.getSession(false); 
+                  if (session != null) {
+                      session.invalidate();
+                  }
+        
+                  Cookie[] cookies = request.getCookies();
+                  if (cookies != null) {
+                      for (Cookie cookie : cookies) {
+                          if ("user".equals(cookie.getName())) {
+                              cookie.setMaxAge(0); 
+                              response.addCookie(cookie);
+                          }
+                      }
+                  }
+          
+                  
+                  response.sendRedirect("login.html");
     }
+
+    
 }
